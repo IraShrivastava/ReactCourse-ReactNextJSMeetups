e52d926ea2e0481a687df0e4260cf50b7aee15a5
IraShrivastava <ira.shrivastava0211@gmail.com>
2021-07-31 20:35:52 +0530

    initial commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,30 @@
+import { MongoClient} from 'mongodb'
+import {mongo} from '../../mongocred'
+
+// api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+    if(req.method === 'POST'){
+        const data = req.body;
+
+        //const {title, image, address, description} = data;
+
+        const client = await MongoClient.connect('mongodb+srv://'+mongo.userName+':'+mongo.pass+'@cluster0.9auyg.mongodb.net/meetups?retryWrites=true&w=majority')
+        const db = client.db()
+
+        const meetupsCollection = db.collection('meetups')
+        const result = await meetupsCollection.insertOne(data)
+
+        console.log(result);
+
+        client.close()
+
+        res.status(201).json({
+            message: 'Meetup Inserted!'
+        })
+
+    }
+}
+
+export default handler
